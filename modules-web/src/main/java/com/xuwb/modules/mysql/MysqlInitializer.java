5c38ea9684175b80e6335621eb66504edfdc6a7a
xuwb-b <xuwb-b@glodon.com>
2019-07-12 18:04:40 +0800

    adsd

--- modules-web/src/main/java/com/xuwb/modules/mysql/MysqlInitializer.java
+++ modules-web/src/main/java/com/xuwb/modules/mysql/MysqlInitializer.java
@@ -3,6 +3,9 @@ package com.xuwb.modules.mysql;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Component
 public class MysqlInitializer {
@@ -11,11 +14,24 @@ public class MysqlInitializer {
 
     private static String defaultScriptCharset = "UTF-8";
 
-    public void executeSqlIfNotExistDb(Connection connection,String database){
+    public void executeSqlIfNotExistDb(Connection connection,String database) throws SQLException {
         executeSqlIfNotExistDb(connection,database,defaultSqlResourcePattern,defaultScriptCharset);
     }
 
-    public void executeSqlIfNotExistDb(Connection connection,String database,String sqlSourcePatten,String scriptCharset){
+    public void executeSqlIfNotExistDb(Connection connection,String database,String sqlSourcePatten,String scriptCharset) throws SQLException {
+        //判断数据库是否存在
+        if (!isDbExists(connection,database)){
+            //执行sql目录下的sql语句
+        }
+    }
 
+    private static boolean isDbExists(Connection connection, String database) throws SQLException {
+        Statement stmt = connection.createStatement();
+        String sql = "SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name=\"" + database + "\"";
+        ResultSet rs = stmt.executeQuery(sql);
+        if (rs.next()) {
+            return rs.getInt(1) != 0;
+        }
+        return false;
     }
 }
